dd4fcae19b340343e83b338a6bde5e80f26dac37
revathiptr <104005472+revathiptr@users.noreply.github.com>
2022-05-12 09:47:25 +0530

    Update Links.java

--- Links.java
+++ Links.java
@@ -1,35 +1,39 @@
+
 package week2.day2;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class EditFields {
+public class Links {
 	public static void main(String[] args) throws InterruptedException {
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
-
+		driver.get("http://www.leafground.com/");
 		driver.manage().window().maximize();
-
-		driver.get("http://leafground.com/pages/Edit.html");
-
-		driver.findElement(By.id("email")).sendKeys("nivethaece0798@gmail.com");
-
-
-		WebElement element = driver.findElement(By.xpath("//label[contains(text(),'Append a text and press keyboard tab')]/following-sibling::input"));
-		element.sendKeys("with edit fields",Keys.TAB);
-
-		System.out.println(driver.findElement(By.xpath("//input[@name='username'][1]")).getAttribute("value"));
-
-		driver.findElement(By.xpath("//label[text()='Clear the text']/following-sibling::input")).clear();
-
-		if(driver.findElement(By.xpath("//label[text()='Confirm that edit field is disabled']/following-sibling::input")).isEnabled())
-			System.out.println("Field is enabled");
+		driver.findElement(By.xpath("(//a[contains(@class,'wp-categories')])[3]")).click();
+		driver.findElement(By.linkText("Go to Home Page")).click();
+		String title = driver.getTitle();
+		System.out.println(title);
+		if(title.contains("Playground")) {
+			System.out.println("Home page loaded");
+		}
+		driver.navigate().back();
+		String link = driver.findElement(By.xpath("//a[text()='Find where am supposed to go without clicking me?']")).getAttribute("href");
+		System.out.println("Link: "+link);
+		boolean result = driver.findElement(By.xpath("//a[text()='Verify am I broken?']")).isEnabled();
+		if(result)
+		{
+			System.out.println("Link is enabled");
+		}
 		else
-			System.out.println("Field is disabled");
-		Thread.sleep(5000);
+			System.out.println("Link disabled");
+		//Thread.sleep(2000);
+		//driver.navigate().back();
+		driver.findElement(By.linkText("Go to Home Page")).click();
+		Thread.sleep(2000);
 		driver.close();
 	}
+
 }
